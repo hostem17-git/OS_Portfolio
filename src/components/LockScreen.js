61f68603127b40633e615c4ad25402c13dc607df
hostem17 <vjjais17@gmail.com>
2022-02-12 19:08:41 +0530

    Login functionality added

--- src/components/LockScreen.js
+++ src/components/LockScreen.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Avatar, Button } from '@mui/material'
+import { useDispatch } from "react-redux"
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { login } from "../features/appSlice"
 
 
 function LockScreen() {
     const [name, setName] = useState("");
     const bg_image_source = "https://unsplash.com/photos/Z1BEsigS0TI/download?ixid=MnwxMjA3fDB8MXx0b3BpY3x8Q0R3dXdYSkFiRXd8fHx8fDJ8fDE2NDQ2NjQ3Njg&force=true&w=1920"
-
+    const dispatch = useDispatch()
     const handleLogin = (e) => {
         e.preventDefault();
+        dispatch(login({
+            name: { name }
+        }))
         console.log("clicked")
     }
 
@@ -27,17 +32,20 @@ function LockScreen() {
                 <p>Hello {name}!</p>
                 {/* Input Field */}
                 <LockScreenInput>
-                    <input placeholder='Enter Your Name' value={name} onChange={(e) => setName(e.target.value)} />
-                    <Button type="submit" onClick={handleLogin}>
-                        <ArrowForwardIcon />
-                    </Button>
+                    <form>
+                        <input placeholder='Enter Your Name' value={name} onChange={(e) => setName(e.target.value)} />
+
+                        <Button type="submit" onClick={handleLogin}>
+                            <ArrowForwardIcon />
+                        </Button>
+                    </form>
 
                     {/* {{box-shadow: 0px 1px 5px white;}} */}
                 </LockScreenInput>
 
             </LockScreenData>
 
-        </LockScreenContainer>
+        </LockScreenContainer >
     )
 }
 
@@ -90,7 +98,7 @@ const LockScreenInput = styled.div`
     display:flex;
     align-items:center;
     background-color: white;
-    >input{
+    >form>input{
         padding:5px;
         border-radius: 5px ;
         border:none;
---
+++ src/features/appSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+
+// The function below is called a thunk and allows us to perform async logic. It
+// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
+// will call the thunk with the `dispatch` function as the first argument. Async
+// code can then be executed and other actions can be dispatched. Thunks are
+// typically used to make async requests.
+
+export const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+        loggedIn: false,
+        name: null
+    },
+    // The `reducers` field lets us define reducers and generate associated actions
+    reducers: {
+        login: (state, action) => {
+            state.loggedIn = true;
+            state.name = action.payload.name;
+        },
+        logout: (state) => {
+            state.loggedIn = false;
+        }
+    },
+});
+
+export const { login, logout } = appSlice.actions;
+
+// The function below is called a selector and allows us to select a value from
+// the state. Selectors can also be defined inline where they're used instead of
+// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
+export const checkloginStatus = (state) => state.loggedIn.value;
+export const getUserName = (state) => state.name.value;
+
+// We can also write thunks by hand, which may contain both sync and async logic.
+// Here's an example of conditionally dispatching actions based on current state.
+
+export default appSlice.reducer;
