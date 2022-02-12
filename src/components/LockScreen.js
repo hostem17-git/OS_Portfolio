1d90d50e0b64529eef59bb9d7689dd3da8947b0a
hostem17 <vjjais17@gmail.com>
2022-02-12 18:49:55 +0530

    Lockscreen UI complete

--- src/components/LockScreen.js
+++ src/components/LockScreen.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Avatar } from '@mui/material'
+import { Avatar, Button } from '@mui/material'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 
 function LockScreen() {
-
+    const [name, setName] = useState("");
     const bg_image_source = "https://unsplash.com/photos/Z1BEsigS0TI/download?ixid=MnwxMjA3fDB8MXx0b3BpY3x8Q0R3dXdYSkFiRXd8fHx8fDJ8fDE2NDQ2NjQ3Njg&force=true&w=1920"
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("clicked")
+    }
+
     return (
         <LockScreenContainer>
             <img src={bg_image_source} alt="background" />
 
-            <LockScreenInput>
+            <LockScreenData>
                 {/* Avatar */}
                 <Avatar
                     alt="Login Avatar"
                     sx={{ width: 100, height: 100 }}
-                />
+                >{name[0]}</Avatar>
                 {/* Hello */}
-                <p>Hello!</p>
+                <p>Hello {name}!</p>
                 {/* Input Field */}
+                <LockScreenInput>
+                    <input placeholder='Enter Your Name' value={name} onChange={(e) => setName(e.target.value)} />
+                    <Button type="submit" onClick={handleLogin}>
+                        <ArrowForwardIcon />
+                    </Button>
 
-                <input placeholder='Enter Your Name' />
-            </LockScreenInput>
+                    {/* {{box-shadow: 0px 1px 5px white;}} */}
+                </LockScreenInput>
+
+            </LockScreenData>
 
         </LockScreenContainer>
     )
@@ -42,10 +56,9 @@ const LockScreenContainer = styled.div`
         /* object-fit:contain; */
         width:100%;
     }
-
     `;
 
-const LockScreenInput = styled.div`
+const LockScreenData = styled.div`
     position:absolute;
     top:50%;
     left:50%;
@@ -63,18 +76,37 @@ const LockScreenInput = styled.div`
         color:white;
     }
 
+    
+    >.MuiAvatar-root{
+        font-size: 10vh;
+        text-transform: capitalize;
+    }
+
+
+`;
+
+const LockScreenInput = styled.div`
+    padding: 0%;;
+    display:flex;
+    align-items:center;
+    background-color: white;
     >input{
-        padding:8px;
+        padding:5px;
         border-radius: 5px ;
         border:none;
         outline:none;
     }
 
-    >input:focus{  
+    :focus{
+        box-shadow: 0px 1px 5px white;
+    }
+    :hover{
         box-shadow: 0px 1px 5px white;
     }
 
+    >.MuiAvatar-root:hover{
+        background-color: red;
+    }
 `;
-
 
 
